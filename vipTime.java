736208434d22f002da679366d5e520d97a437e13
DY\ollie.yang <yangjaunting@gmail.com>
2024-10-24 15:46:18 +0800

    +1

--- vipTime.java
+++ vipTime.java
@@ -24,7 +24,7 @@ public class vipTime
       upgradeDeadlineAtTemp += resetDayTime;
       upgradeDeadlineStartAtTemp += resetDayTime;
     }
-    //改12
+    //改12  +  1
     upgradeDeadlineAt = upgradeDeadlineAtTemp;
     upgradeDeadlineStartAt = upgradeDeadlineStartAtTemp;
   }
