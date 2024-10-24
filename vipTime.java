7ac14a34e0e7a976b19d91c302546d3bea7895f3
DY\ollie.yang <yangjaunting@gmail.com>
2024-10-24 15:14:09 +0800

    1

---
+++ vipTime.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
+public class vipTime
+{
+  protected static Long updatedAt;
+  public static final long DAY28 = TimeUnit.DAYS.toMillis(28);
+  // 保級期限
+  private static long regularDeadlineAt;
+
+  // 晉級期限
+  private static long upgradeDeadlineAt;
+
+  // 晉級起始時間
+  private static long upgradeDeadlineStartAt;
+
+  public static void generateDeadlineAt(int resetDay) {
+    var resetDayTime = TimeUnit.DAYS.toMillis(resetDay);
+    regularDeadlineAt =updatedAt + DAY28;
+    long nowTime = System.currentTimeMillis();
+    long upgradeDeadlineAtTemp = updatedAt + resetDayTime;
+    long upgradeDeadlineStartAtTemp = updatedAt;
+    while (nowTime > upgradeDeadlineAtTemp) {
+      upgradeDeadlineAtTemp += resetDayTime;
+      upgradeDeadlineStartAtTemp += resetDayTime;
+    }
+    upgradeDeadlineAt = upgradeDeadlineAtTemp;
+    upgradeDeadlineStartAt = upgradeDeadlineStartAtTemp;
+  }
+  public static void main(String[] args) {
+    updatedAt=1620305600000L;
+    generateDeadlineAt(26);
+    System.out.println("保級期限regularDeadlineAt:"+regularDeadlineAt);
+    System.out.println("晉級期限upgradeDeadlineAt:"+upgradeDeadlineAt);
+    System.out.println("晉級起始時間upgradeDeadlineStartAt:"+upgradeDeadlineStartAt);
+  }
+
+}
